58a0f87d4fd6d23ca9372ad900738a084a694387
shashank-386045 <shashank.nuthalapati06@gmail.com>
2026-03-24 18:38:40 +0530

    UC4:Portfolio Return Sorting

--- src/week3assignments.java
+++ src/week3assignments.java
@@ -1,12 +1,28 @@
 import java.util.*;
 
 class week3assignments {
-    static void merge(int[] arr, int l, int m, int r) {
-        int[] temp = new int[r - l + 1];
+    static class Asset {
+        String name;
+        int returnRate;
+        int volatility;
+
+        Asset(String n, int r, int v) {
+            name = n;
+            returnRate = r;
+            volatility = v;
+        }
+
+        public String toString() {
+            return name + ":" + returnRate;
+        }
+    }
+
+    static void merge(Asset[] arr, int l, int m, int r) {
+        Asset[] temp = new Asset[r - l + 1];
         int i = l, j = m + 1, k = 0;
 
         while (i <= m && j <= r) {
-            if (arr[i] <= arr[j]) temp[k++] = arr[i++];
+            if (arr[i].returnRate <= arr[j].returnRate) temp[k++] = arr[i++];
             else temp[k++] = arr[j++];
         }
 
@@ -16,7 +32,7 @@ class week3assignments {
         for (i = l, k = 0; i <= r; i++, k++) arr[i] = temp[k];
     }
 
-    static void mergeSort(int[] arr, int l, int r) {
+    static void mergeSort(Asset[] arr, int l, int r) {
         if (l < r) {
             int m = (l + r) / 2;
             mergeSort(arr, l, m);
@@ -25,20 +41,21 @@ class week3assignments {
         }
     }
 
-    static int partition(int[] arr, int l, int r) {
-        int pivot = arr[r];
+    static int partition(Asset[] arr, int l, int r) {
+        Asset pivot = arr[r];
         int i = l - 1;
         for (int j = l; j < r; j++) {
-            if (arr[j] > pivot) {
+            if (arr[j].returnRate > pivot.returnRate ||
+                    (arr[j].returnRate == pivot.returnRate && arr[j].volatility < pivot.volatility)) {
                 i++;
-                int t = arr[i]; arr[i] = arr[j]; arr[j] = t;
+                Asset t = arr[i]; arr[i] = arr[j]; arr[j] = t;
             }
         }
-        int t = arr[i + 1]; arr[i + 1] = arr[r]; arr[r] = t;
+        Asset t = arr[i + 1]; arr[i + 1] = arr[r]; arr[r] = t;
         return i + 1;
     }
 
-    static void quickSort(int[] arr, int l, int r) {
+    static void quickSort(Asset[] arr, int l, int r) {
         if (l < r) {
             int p = partition(arr, l, r);
             quickSort(arr, l, p - 1);
@@ -47,16 +64,16 @@ class week3assignments {
     }
 
     public static void main(String[] args) {
-        int[] arr = {500, 100, 300};
+        Asset[] arr = {
+                new Asset("AAPL", 12, 5),
+                new Asset("TSLA", 8, 7),
+                new Asset("GOOG", 15, 4)
+        };
 
         mergeSort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
 
         quickSort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
-
-        int sum = 0;
-        for (int x : arr) sum += x;
-        System.out.println(sum);
     }
 }
