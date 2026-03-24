a96d5a0600378ddc52e2b5f64445bb0bdaaae99d
shashank-386045 <shashank.nuthalapati06@gmail.com>
2026-03-24 18:40:59 +0530

    UC5:Account ID Lookup in Transaction Logs

--- src/week3assignments.java
+++ src/week3assignments.java
@@ -1,79 +1,38 @@
 import java.util.*;
 
 class week3assignments {
-    static class Asset {
-        String name;
-        int returnRate;
-        int volatility;
-
-        Asset(String n, int r, int v) {
-            name = n;
-            returnRate = r;
-            volatility = v;
-        }
-
-        public String toString() {
-            return name + ":" + returnRate;
-        }
+    static int linearFirst(String[] arr, String key) {
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i].equals(key)) return i;
+        return -1;
     }
 
-    static void merge(Asset[] arr, int l, int m, int r) {
-        Asset[] temp = new Asset[r - l + 1];
-        int i = l, j = m + 1, k = 0;
-
-        while (i <= m && j <= r) {
-            if (arr[i].returnRate <= arr[j].returnRate) temp[k++] = arr[i++];
-            else temp[k++] = arr[j++];
-        }
-
-        while (i <= m) temp[k++] = arr[i++];
-        while (j <= r) temp[k++] = arr[j++];
-
-        for (i = l, k = 0; i <= r; i++, k++) arr[i] = temp[k];
+    static int linearLast(String[] arr, String key) {
+        for (int i = arr.length - 1; i >= 0; i--)
+            if (arr[i].equals(key)) return i;
+        return -1;
     }
 
-    static void mergeSort(Asset[] arr, int l, int r) {
-        if (l < r) {
+    static int binarySearch(String[] arr, String key) {
+        int l = 0, r = arr.length - 1;
+        while (l <= r) {
             int m = (l + r) / 2;
-            mergeSort(arr, l, m);
-            mergeSort(arr, m + 1, r);
-            merge(arr, l, m, r);
+            if (arr[m].equals(key)) return m;
+            if (arr[m].compareTo(key) < 0) l = m + 1;
+            else r = m - 1;
         }
-    }
-
-    static int partition(Asset[] arr, int l, int r) {
-        Asset pivot = arr[r];
-        int i = l - 1;
-        for (int j = l; j < r; j++) {
-            if (arr[j].returnRate > pivot.returnRate ||
-                    (arr[j].returnRate == pivot.returnRate && arr[j].volatility < pivot.volatility)) {
-                i++;
-                Asset t = arr[i]; arr[i] = arr[j]; arr[j] = t;
-            }
-        }
-        Asset t = arr[i + 1]; arr[i + 1] = arr[r]; arr[r] = t;
-        return i + 1;
-    }
-
-    static void quickSort(Asset[] arr, int l, int r) {
-        if (l < r) {
-            int p = partition(arr, l, r);
-            quickSort(arr, l, p - 1);
-            quickSort(arr, p + 1, r);
-        }
+        return -1;
     }
 
     public static void main(String[] args) {
-        Asset[] arr = {
-                new Asset("AAPL", 12, 5),
-                new Asset("TSLA", 8, 7),
-                new Asset("GOOG", 15, 4)
-        };
+        String[] arr = {"accA", "accB", "accB", "accC"};
 
-        mergeSort(arr, 0, arr.length - 1);
-        System.out.println(Arrays.toString(arr));
+        System.out.println(linearFirst(arr, "accB"));
+        System.out.println(linearLast(arr, "accB"));
+        System.out.println(binarySearch(arr, "accB"));
 
-        quickSort(arr, 0, arr.length - 1);
-        System.out.println(Arrays.toString(arr));
+        int count = 0;
+        for (String s : arr) if (s.equals("accB")) count++;
+        System.out.println(count);
     }
 }
